3c792341090d64d79f39b60c27e35d05a9b3cae7
julianty <alexanderjulianty@gmail.com>
2022-11-22 17:45:08 -0800

    Adds firestore to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { collection } from "firebase/firestore";
+import { useEffect } from "react";
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_apiKey,
+//   authDomain: process.env.REACT_APP_authDomain,
+//   projectId: process.env.REACT_APP_projectId,
+//   storageBucket: process.env.REACT_APP_storageBucket,
+//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//   appId: process.env.REACT_APP_appId,
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAllAim2pin7FeCp_v5GRFg4WQv1iboObI",
+  authDomain: "workout-tracker-2d69d.firebaseapp.com",
+  projectId: "workout-tracker-2d69d",
+  storageBucket: "workout-tracker-2d69d.appspot.com",
+  messagingSenderId: "781275830167",
+  appId: "1:781275830167:web:be75eaac33474c22d656d2",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(db, "test", "testDoc");
+      const testDoc = await getDoc(docRef);
+      if (testDoc.exists()) {
+        console.log(testDoc.data());
+      }
+    };
+    fetchData().catch(console.err);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
