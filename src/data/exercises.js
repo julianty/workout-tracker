01ae3f9fed03cbc7a8ca8ee3f3402c45db49d0b6
julianty <alexanderjulianty@gmail.com>
2023-01-09 16:23:00 -0800

    Creates dummy data

---
+++ src/data/exercises.js
@@ -0,0 +1,19 @@
+var exercises = {
+  data: [
+    {
+      name: "Barbell Squat",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+
+    {
+      name: "Barbell Bench Press",
+      muscles: ["Pectorals", "Deltoids"],
+    },
+    {
+      name: "Barbell Deadlift",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+  ],
+};
+
+export default exercises;
