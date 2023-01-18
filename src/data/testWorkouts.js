6939979837a0c0b6e31e02bed6ac77e2b21894a6
julianty <alexanderjulianty@gmail.com>
2023-01-18 12:03:10 -0800

    Allows for uploading test data to database

---
+++ src/data/testWorkouts.js
@@ -0,0 +1,50 @@
+const testWorkouts = {
+  testSession1: {
+    timestamp: "2023-01-19T00:00:00+0000",
+    exercises: [
+      {
+        exercise: "squat",
+        sets: "3",
+        reps: "3",
+        weight: 225,
+      },
+      {
+        exercise: "bench press",
+        sets: "3",
+        reps: "5",
+        weight: 185,
+      },
+      {
+        exercise: "deadlift",
+        sets: "3",
+        reps: "3",
+        weight: 315,
+      },
+    ],
+  },
+  testSession2: {
+    timestamp: "2023-01-20T00:00:00+0000",
+    exercises: [
+      {
+        exercise: "squat",
+        sets: "3",
+        reps: "3",
+        weight: 235,
+      },
+      {
+        exercise: "bench press",
+        sets: "3",
+        reps: "5",
+        weight: 200,
+      },
+      {
+        exercise: "deadlift",
+        sets: "3",
+        reps: "3",
+        weight: 315,
+      },
+    ],
+  },
+};
+
+export default testWorkouts;
---
+++ src/data/uploadTestData.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+import testUserData from "./testWorkouts.js";
+import testWorkouts from "./testWorkouts.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAllAim2pin7FeCp_v5GRFg4WQv1iboObI",
+  authDomain: "workout-tracker-2d69d.firebaseapp.com",
+  projectId: "workout-tracker-2d69d",
+  storageBucket: "workout-tracker-2d69d.appspot.com",
+  messagingSenderId: "781275830167",
+  appId: "1:781275830167:web:be75eaac33474c22d656d2",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function uploadTestData() {
+  await setDoc(doc(db, "testUserData", "Workouts"), testUserData);
+}
+
+export default uploadTestData;
