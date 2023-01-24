5cdb259f56c9cdf26682de8009acdfeeadd6eb50
julianty <alexanderjulianty@gmail.com>
2023-01-24 12:19:27 -0800

    Updates store.js

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import workoutsReducer from "../features/workouts/workoutsSlice";
+export default configureStore({
+  reducer: {
+    workouts: workoutsReducer,
+  },
+});
