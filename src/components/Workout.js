ba793b1989efc38a88dd3a9a3c63c9039a6f8878
julianty <alexanderjulianty@gmail.com>
2023-01-24 10:43:36 -0800

    Allows for editing content on tool page

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -3,14 +3,39 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
+import { useState } from "react";
+import uniqid from "uniqid";
+
 function Exercise(props) {
+  function EditableInput(props) {
+    return (
+      <input
+        className="form-control-plaintext"
+        type={props.type}
+        onChange={changeHandler}
+        defaultValue={props.defaultValue}
+      ></input>
+    );
+  }
+  function changeHandler(e) {
+    // TODO: on change, save data to firestore
+    console.log(e);
+  }
   const exerciseData = props.exerciseData;
   return (
     <Row>
-      <Col>{exerciseData.exercise}</Col>
-      <Col>{exerciseData.sets}</Col>
-      <Col>{exerciseData.reps}</Col>
-      <Col>{exerciseData.weight}</Col>
+      <Col>
+        <EditableInput type="text" defaultValue={exerciseData.exercise} />
+      </Col>
+      <Col>
+        <EditableInput type="number" defaultValue={exerciseData.sets} />
+      </Col>
+      <Col>
+        <EditableInput type="number" defaultValue={exerciseData.reps} />
+      </Col>
+      <Col>
+        <EditableInput type="number" defaultValue={exerciseData.weight} />
+      </Col>
     </Row>
   );
 }
@@ -29,15 +54,31 @@ function WorkoutHeading() {
 function Workout(props) {
   const data = props.workoutData;
   const timestamp = new Date(data.timestamp);
-  const exercisesArr = data.exercises;
+
+  const [exercisesArr, setExercisesArr] = useState(data.exercises);
+  // const exercisesArr = data.exercises;
+
+  function addNewExercise() {
+    // console.log("add new exercise");
+    // console.log(typeof exercisesArr);
+    const exercisesArrCopy = [...exercisesArr];
+    exercisesArrCopy.push({
+      exercise: "Click to edit",
+      reps: "0",
+      sets: "0",
+      weight: "0",
+    });
+    setExercisesArr(exercisesArrCopy);
+  }
+
   return (
     <Container>
       <h4>{timestamp.toDateString()}</h4>
       <WorkoutHeading />
       {exercisesArr.map((exerciseData) => (
-        <Exercise exerciseData={exerciseData} />
+        <Exercise exerciseData={exerciseData} key={uniqid()} />
       ))}
-      <Button>Add exercise</Button>
+      <Button onClick={addNewExercise}>Add exercise</Button>
     </Container>
   );
 }
