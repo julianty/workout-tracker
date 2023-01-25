982ff787e29aab076ae3ee5dee711cb778a4944f
julianty <alexanderjulianty@gmail.com>
2023-01-25 11:17:08 -0800

    Adds linebreak for formatting

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import workoutsReducer from "../features/workouts/workoutsSlice";
+
 export default configureStore({
   reducer: {
     workouts: workoutsReducer,
