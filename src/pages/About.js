3677434c0033c21840fcb63e5bf95a3ac200bd43
julianty <alexanderjulianty@gmail.com>
2023-01-27 12:06:17 -0800

    Adds content to page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,19 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
 function About() {
-  return <h1>About Page</h1>;
+  return (
+    <Container>
+      <h1>About Page</h1>
+      <Row>
+        <Col>
+          This tool is meant as a easy way to track my workouts and hopefully
+          act as a tool to help create workout plans in the future.
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <Container>
+      <h1>Home Page</h1>
+      <Row>
+        <Col>
+          Welcome to the workout tracker I've been working on. Please let me
+          know if you have any feature suggestions, bugs you encouter, layout
+          improvements etc.
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
