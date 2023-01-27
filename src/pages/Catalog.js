d725a470d0b6c1b17afd4d031e122f82465d1399
julianty <alexanderjulianty@gmail.com>
2023-01-27 12:19:08 -0800

    Changes formatting for better readability

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,26 +1,31 @@
 import Container from "react-bootstrap/esm/Container.js";
 import exercises from "../data/exercises.js";
+import uniqid from "uniqid";
 
 function fetchData() {
+  // TODO fetch data from firestore
   return exercises.data;
 }
 function Catalog() {
   let exerciseList = fetchData();
-  console.log(exerciseList);
   return (
     <Container className="d-flex flex-column justify-content-start">
       {exerciseList.map((exercise) => {
         return (
           <Container
+            key={uniqid()}
             className="d-flex flex-column"
             style={{ textAlign: "left" }}
           >
-            <h1>{exercise.name}</h1>
-            {exercise.muscles.map((muscle) => (
-              <div className="ms-3">
-                <h3>{muscle}</h3>
+            <h3>{exercise.name}</h3>
+            <div className="d-flex align-items-center ms-3">
+              <div>Muscles involved: {exercise.muscles.join(", ")}</div>
+            </div>
+            {/* {exercise.muscles.map((muscle) => (
+              <div key={uniqid()} className="ms-3">
+                <p>{muscle}</p>
               </div>
-            ))}
+            ))} */}
           </Container>
         );
       })}
