251640bc13a47380afaedd32eb93b3a71797ca38
julianty <alexanderjulianty@gmail.com>
2023-01-30 14:35:13 -0800

    Refactors navlinks to preserve data between routes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import { Link } from "react-router-dom";
 function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
@@ -10,10 +10,18 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/tool">Tool</Nav.Link>
-            <Nav.Link href="/catalog">Catalog</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/tool">
+              Tool
+            </Nav.Link>
+            <Nav.Link as={Link} to="/catalog">
+              Catalog
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
