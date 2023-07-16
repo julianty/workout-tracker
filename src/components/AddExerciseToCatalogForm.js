b41ea83e756acd4474860d6841313f3488c96d06
julianty <alexanderjulianty@gmail.com>
2023-07-16 16:10:13 -0700

    Moves form in catalog to components folder

---
+++ src/components/AddExerciseToCatalogForm.js
@@ -0,0 +1,51 @@
+import { Form, Field } from "react-final-form";
+import { Row, Col, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+function AddExerciseToCatalogForm() {
+  const dispatch = useDispatch();
+  const onSubmit = async (values) => {
+    // console.log(values);
+    // Need to add some form validation here
+    dispatch({ type: "workouts/addExerciseToCatalog", payload: values });
+  };
+  return (
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, form }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <Row>
+              <Col md="auto">
+                <label>Exercise Name</label>
+              </Col>
+              <Col md="auto">
+                <Field
+                  name="exerciseName"
+                  component="input"
+                  type="text"
+                  placeholder="Barbell Squat"
+                />
+              </Col>
+              <Col md="auto">
+                <label>Muscles Involed {"(separate with commas)"}</label>
+              </Col>
+              <Col md="auto">
+                <Field
+                  name="muscles"
+                  component="input"
+                  type="text"
+                  placeholder="Quadriceps, Pectorals, Triceps"
+                />
+              </Col>
+              <Col md="2">
+                <Button type="submit">Submit</Button>
+              </Col>
+            </Row>
+          </form>
+        );
+      }}
+    ></Form>
+  );
+}
+
+export default AddExerciseToCatalogForm;
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,57 +1,16 @@
 import Container from "react-bootstrap/esm/Container.js";
 import uniqid from "uniqid";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Row, Col, Button, FormLabel } from "react-bootstrap/";
-import { Form, Field } from "react-final-form/";
+import AddExerciseToCatalogForm from "../components/AddExerciseToCatalogForm";
 function Catalog() {
   const exerciseList = useSelector((state) => state.workouts.catalog);
-  const dispatch = useDispatch();
-  const onSubmit = async (values) => {
-    // console.log(values);
-    // Need to add some form validation here
-    dispatch({ type: "workouts/addExerciseToCatalog", payload: values });
-  };
   return (
     <Container className="d-flex flex-column justify-content-start">
       <Row>Add an exercise to the catalog</Row>
       <Row>
         <Col>
-          <Form
-            onSubmit={onSubmit}
-            render={({ handleSubmit, form }) => {
-              return (
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col md="auto">
-                      <label>Exercise Name</label>
-                    </Col>
-                    <Col md="auto">
-                      <Field
-                        name="exerciseName"
-                        component="input"
-                        type="text"
-                        placeholder="Barbell Squat"
-                      />
-                    </Col>
-                    <Col md="auto">
-                      <label>Muscles Involed {"(separate with commas)"}</label>
-                    </Col>
-                    <Col md="auto">
-                      <Field
-                        name="muscles"
-                        component="input"
-                        type="text"
-                        placeholder="Quadriceps, Pectorals, Triceps"
-                      />
-                    </Col>
-                    <Col md="2">
-                      <Button type="submit">Submit</Button>
-                    </Col>
-                  </Row>
-                </form>
-              );
-            }}
-          ></Form>
+          <AddExerciseToCatalogForm />
         </Col>
       </Row>
       <Row>
