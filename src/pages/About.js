74f754669a03aee7ed7dd710f831ddec0c7f67b8
julianty <alexanderjulianty@gmail.com>
2023-07-18 12:18:37 -0700

    Adds some more content to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,6 +12,23 @@ function About() {
           act as a tool to help create workout plans in the future.
         </Col>
       </Row>
+      <Row>
+        <Col>
+          This website was built with React and uses Firebase for hosting,
+          authentication, and data. I also use Redux for state management and
+          bootstrap for styling.
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          If you like, you can{" "}
+          <a href="https://github.com/julianty/workout-tracker">
+            view the project on Github
+          </a>{" "}
+          or visit{" "}
+          <a href="https://alexanderjulianty.com">my personal website</a>
+        </Col>
+      </Row>
     </Container>
   );
 }
